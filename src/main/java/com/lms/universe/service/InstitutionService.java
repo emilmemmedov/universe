fd4750eec08c355adffb02a7df31ccd889d792cd
Emil Məmmədov <emill.memmedovv@gmail.com>
2021-11-21 16:22:42 +0400

    dynamic controller done

--- src/main/java/com/lms/universe/service/InstitutionService.java
+++ src/main/java/com/lms/universe/service/InstitutionService.java
@@ -1,18 +1,5 @@
 package com.lms.universe.service;
 
-import com.lms.universe.dto.InstitutionCreateDto;
-import com.lms.universe.dto.InstitutionGetDto;
-import com.lms.universe.exception.ApiException;
-import com.lms.universe.model.Institution;
-import com.lms.universe.model.InstitutionType;
-import com.lms.universe.repository.InstitutionRepository;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 public interface InstitutionService {
 
 }
